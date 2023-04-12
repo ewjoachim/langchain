d9158040597927480d8130512bdec5b0ab85b6ad
Joachim Jablon <joachim@botify.com>
2023-04-12 10:57:20 +0200

    Remove incompatible libs

--- langchain/vectorstores/__init__.py
+++ langchain/vectorstores/__init__.py
@@ -7,7 +7,7 @@ from langchain.vectorstores.elastic_vector_search import ElasticVectorSearch
 from langchain.vectorstores.faiss import FAISS
 from langchain.vectorstores.milvus import Milvus
 from langchain.vectorstores.opensearch_vector_search import OpenSearchVectorSearch
-from langchain.vectorstores.pinecone import Pinecone
+# from langchain.vectorstores.pinecone import Pinecone
 from langchain.vectorstores.qdrant import Qdrant
 from langchain.vectorstores.weaviate import Weaviate
 
@@ -15,7 +15,7 @@ __all__ = [
     "ElasticVectorSearch",
     "FAISS",
     "VectorStore",
-    "Pinecone",
+    # "Pinecone",
     "Weaviate",
     "Qdrant",
     "Milvus",
